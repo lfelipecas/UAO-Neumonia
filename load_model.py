147a41e4475abd8938e75d3106798244abe82d4c
lfelipecas <lufecasta@gmail.com>
2024-07-25 11:30:46 -0500

    Update in load_model.py to improve model loading from a current directory

--- load_model.py
+++ load_model.py
@@ -1,7 +1,11 @@
+import os
 import tensorflow as tf
 
 def model_fun():
-    # The following path is an example of a local path used during development.
-    # Replace it with your own path to the model file.
-    model_path = r"H:\My Drive\UAO_EIA\5_Desarrollo_Proyectos_IA\Modulo_1_Introduccion_Desarrollo_Software\UAO-Neumonia\conv_MLP_84.h5"
+    # Obtiene el directorio del archivo actual
+    current_dir = os.path.dirname(__file__)
+    
+    # Construye la ruta al archivo del modelo en relación al directorio actual
+    model_path = os.path.join(current_dir, 'conv_MLP_84.h5')
+    
     return tf.keras.models.load_model(model_path)
